e23767f2904f7ed84a26119fabccf6bc48f6e60e
Alexander Uddfeldt <alexander.uddfeldt@gaia.se>
2017-01-16 14:15:59 +0100

    added files

---
+++ TestApp/TestApp.Droid/MainActivity.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+using Microsoft.Azure.Mobile;
+using Microsoft.Azure.Mobile.Analytics;
+using Microsoft.Azure.Mobile.Crashes;
+
+namespace TestApp.Droid
+{
+    [Activity(Label = "TestApp", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(bundle);
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+
+            MobileCenter.Configure("6a4662d1-50a7-442d-952c-40150437d88b");
+
+            LoadApplication(new App());
+        }
+    }
+}
+
---
+++ TestApp/TestApp/App.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Azure.Mobile;
+using Microsoft.Azure.Mobile.Analytics;
+using Microsoft.Azure.Mobile.Crashes;
+
+using Xamarin.Forms;
+
+namespace TestApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            MobileCenter.Start(typeof(Analytics), typeof(Crashes));
+
+            InitializeComponent();
+
+            MainPage = new TestApp.MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
